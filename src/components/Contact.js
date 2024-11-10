d500b0c98a2a912336cb06f9cc1df841d56c7d40
Isaac <isaac.roseman.frame@gmail.com>
2024-11-10 00:46:30 -0500

    .

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,28 +9,30 @@ export default function Contact() {
 	}
 	return (
 		<div className="contact-container">
-			<h1>Reach out now:</h1>
-			<form onSubmit={handleSubmit}>
-				<div className="email-section">
-					<input
-						id="email"
-						type="email"
-						name="email"
-						placeholder="Email Address"
+			<div className="contact-content">
+				<h1>Let's chat!</h1>
+				<form onSubmit={handleSubmit}>
+					<div className="email-section">
+						<input
+							id="email"
+							type="email"
+							name="email"
+							placeholder="Email Address"
+						/>
+					</div>
+					<div className="message-section">
+						<textarea id="message" name="message" placeholder="Message" />
+					</div>
+					<ValidationError
+						prefix="Message"
+						field="message"
+						errors={state.errors}
 					/>
-				</div>
-				<div className="message-section">
-					<textarea id="message" name="message" placeholder="Message" />
-				</div>
-				<ValidationError
-					prefix="Message"
-					field="message"
-					errors={state.errors}
-				/>
-				<button type="submit" disabled={state.submitting}>
-					Submit
-				</button>
-			</form>
+					<button type="submit" disabled={state.submitting}>
+						Submit
+					</button>
+				</form>
+			</div>
 		</div>
 	);
 }
