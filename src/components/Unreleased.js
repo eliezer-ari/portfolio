b05a4e5b99cee48e58e1694123d50394b007d13d
eliezer-ari <rosemaneliezer@gmail.com>
2026-08-11 19:07:03 -0400

    .

--- src/components/Unreleased.js
+++ src/components/Unreleased.js
@@ -17,7 +17,14 @@ const TRACKS = [
 		src: "https://portfoliomusic.s3.us-east-1.amazonaws.com/devotional+track+(dark+mix).flac",
 		fallbackSrc: "https://portfoliomusic.s3.us-east-1.amazonaws.com/devotional+track+(dark+mix).wav",
 		title: "Devotional Track (Dark Mix)",
-		titleBadge: "LOSSLESS",
+		artist: "Ariana Roseman",
+	},
+	{
+		id: "devotionaldark",
+		src: "https://portfoliomusic.s3.us-east-1.amazonaws.com/devotional+track+(updated+mix).flac",
+		fallbackSrc: "https://portfoliomusic.s3.us-east-1.amazonaws.com/devotional+track+(updated+mix).wav",
+		title: "Devotional Track (Updated Mix)",
+		titleBadge: "New!",
 		artist: "Ariana Roseman",
 	},
 ];
